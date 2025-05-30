ed6d618b41c48c3399dcf03b25d1c4558c6f6147
James Wellnitz <wellnitz.james@gmail.com>
2025-05-30 12:22:38 -0400

    update-todo

--- src/deli/dels/compound.py
+++ src/deli/dels/compound.py
@@ -1,5 +1,64 @@
 """for classes relating to DEL compounds"""
 
+import abc
+
+
+class ChemicalCompoundException(Exception):
+    """for errors related to chemical compounds"""
+
+    pass
+
+
+class BaseCompound(abc.ABC):
+    """
+    Base compound class
+
+    This class is used to represent a compound in a DEL.
+    It contains the compound ID, library ID, and building blocks.
+    """
+
+    def __init__(self, compound_id: str):
+        """
+        Initialize the BaseCompound object.
+
+        Parameters
+        ----------
+        compound_id: str
+            The ID of the compound.
+        """
+        self.compound_id = compound_id
+
+    def __eq__(self, other):
+        """Check if two LowMemDELCompound objects are equal."""
+        if not isinstance(other, LowMemDELCompound):
+            return False
+        return self.compound_id == other.compound_id
+
+    def __hash__(self):
+        """Return the hash of the object."""
+        return hash(self.compound_id)
+
+    def __repr__(self):
+        """Return a string representation of the object."""
+        return self.compound_id
+
+    @abc.abstractmethod
+    def get_smiles(self) -> str:
+        """
+        Get the SMILES representation of the full compound
+
+        Returns
+        -------
+        str
+
+        Raises
+        ------
+        ChemicalCompoundException
+            if the SMILES cannot be generated with provided data
+            and is missing
+        """
+        raise NotImplementedError
+
 
 class LowMemDELCompound:
     """
@@ -30,7 +89,7 @@ class LowMemDELCompound:
             The IDs of the building blocks.
         building_block_smis: list[str | None] | None, default None
             The SMILES representations of the building blocks.
-            must be same length as building_blocks_ids if not None
+            Must be the same length as building_blocks_ids if not None
         enumerated_smi: str | None, default None
             The enumerated SMILES representation of the compound.
             If unknown, set to None.
@@ -40,17 +99,3 @@ class LowMemDELCompound:
         self.building_blocks_ids = building_blocks_ids
         self.building_block_smiles = building_block_smis
         self.enumerated_smi = enumerated_smi
-
-    def __eq__(self, other):
-        """Check if two LowMemDELCompound objects are equal."""
-        if not isinstance(other, LowMemDELCompound):
-            return False
-        return self.compound_id == other.compound_id
-
-    def __hash__(self):
-        """Return the hash of the object."""
-        return hash(self.compound_id)
-
-    def __repr__(self):
-        """Return a string representation of the object."""
-        return self.compound_id
--- todo.md
+++ todo.md
@@ -1,17 +1,16 @@
-- [ ] work on k-mer
+After running, the merging of the decode result is too slow when in parallel.
+This is because i'm using pickle.
+I need to come up with a way to avoid that.
 
-Need to figure out how I want to account for anchoring of reads...
-maybe for now I can just skip it and move on and cycle back for speed later
+My thoughts are to make a new class, the isn't "CalledBarcode" but just "DELCompound". This
+will be what the decoder returns.
 
-Three different alignment modes
-- [ ] Align to some fixed region in the barcode (if long enough) and call library then BBs
-This one seems like it might be hard to generalize if a bunch of different schemas are used
-- [ ] library demultiplex first then align and decode
-- [ ] kmer alignment algorithm (numeric based?)
+In turn, this will have functions that let you load and save raw text representations of the
+compounds so we can reload them with library support later.
 
-Need to make sure all library tags are at the front, need logic to reverse library barcodes if
-needed to assert this logic
-
-add headpiece part to barcode schema
-
-think about adding mappy to make things go faster
+Also, the degen object needs to stop hashing on the compounds themselves and start
+working on the ids as strings instead.
+The UMI version should just be a nested dict, where the compound count is a list of umis.
+This way, we can save the output as a json which will be easier to merge after the fact cause its is just raw text.
+Then we can use some other util to load all the DELCompound objects if we need them (for example to generate SMILES).
+This will be way better than using pickles.
