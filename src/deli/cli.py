76019caebdc937bc55df5ee8be9c28023a621b43
lerakaneva <lerakaneva@gmail.com>
2025-06-03 16:03:38 -0400

    Uncomment analysis code

--- src/deli/cli.py
+++ src/deli/cli.py
@@ -1,5 +1,6 @@
 """command line functions for deli"""
 
+import ast
 import configparser
 import datetime
 import enum
@@ -10,6 +11,11 @@ from collections import defaultdict
 from pathlib import Path
 
 import click
+import pandas as pd
+import yaml
+
+from deli.analysis.analysis_report_gen import generate_report
+from deli.analysis.cube_class import DELi_Cube
 
 from deli.configure import (
     DeliDataDirError,
@@ -584,178 +590,177 @@ def cube_from_counter(
     )
 
 
-# this is broken still because the analysis code has import issues
-# removing more now will fix in new branch
-# @cli.command(name="analyze")
-# @click.option(
-#     "--config",
-#     type=click.Path(exists=True, dir_okay=False),
-#     required=True,
-#     help="Path to the YAML config file",
-# )
-# def analyze(config):
-#     """
-#     Perform DEL analysis based on the provided YAML configuration file.
-#
-#     CONFIG is the path to the YAML configuration file.
-#     """
-#     print("Analysis started with config:", config)
-#
-#     def create_output_dir(output_dir):
-#         if not os.path.exists(output_dir):
-#             os.makedirs(output_dir)
-#         return output_dir
-#
-#     def create_dated_output_dir(base_output_dir, name_suffix=""):
-#         output_dir = create_output_dir(base_output_dir)
-#         date_str = datetime.datetime.now().strftime("%Y%m%d")
-#         dated_output_dir = os.path.join(output_dir, f"{date_str}_{name_suffix}")
-#         if not os.path.exists(dated_output_dir):
-#             os.makedirs(dated_output_dir)
-#         return dated_output_dir
-#
-#     def load_config(config_path):
-#         with open(config_path, "r") as file:
-#             return yaml.safe_load(file)
-#
-#     def read_dict_from_file(file_path):
-#         with open(file_path, "r") as f:
-#             content = f.read()
-#             return ast.literal_eval(content)
-#
-#     config = load_config(config)
-#     output_dir_base = config["general"].get("output_dir", "output")
-#     output_dir = create_dated_output_dir(output_dir_base, "analysis")
-#
-#     data_file = config["general"].get("data", "")
-#     if not data_file:
-#         raise ValueError("Data file must be specified in the YAML config.")
-#     df = pd.read_csv(data_file)
-#
-#     id_col = config["general"].get("ID_col", df.columns[0])
-#     indexes = ast.literal_eval(str(config.get("indexes", {})))
-#     control_cols = ast.literal_eval(str(config.get("control_cols", {})))
-#     raw_indexes = ast.literal_eval(str(config.get("raw_indexes", {})))
-#
-#     indexes_path = os.path.join(output_dir, "indexes.txt")
-#     control_cols_path = os.path.join(output_dir, "control_cols.txt")
-#     raw_indexes_path = os.path.join(output_dir, "raw_indexes.txt")
-#
-#     with open(indexes_path, "w") as file:
-#         file.write(str(indexes))
-#     with open(control_cols_path, "w") as file:
-#         file.write(str(control_cols))
-#     with open(raw_indexes_path, "w") as file:
-#         file.write(str(raw_indexes))
-#
-#     indexes = read_dict_from_file(indexes_path)
-#     control_cols = read_dict_from_file(control_cols_path)
-#     raw_indexes = read_dict_from_file(raw_indexes_path)
-#     if not indexes:
-#         indexes = raw_indexes
-#
-#     cube = DELi_Cube(
-#         df,
-#         id_col,
-#         indexes,
-#         control_cols,
-#         int(config["general"].get("lib_size", 0)),
-#         raw_indexes,
-#     )
-#
-#     if "flags" in config:
-#         flags = config["flags"]
-#         if flags.get("SD_min", False):
-#             nsc_max_dict, sd_min_dict, sampling_depth_dict = cube.SD_min()
-#         if flags.get("NSC_values", False):
-#             cube.NSC_values()
-#         if flags.get("MLE", False):
-#             cube.maximum_likelihood_enrichment_ratio()
-#         if flags.get("Z_score", False):
-#             cube.z_score()
-#         if flags.get("z_score_log_data", False):
-#             cube.z_score_log_data()
-#         if flags.get("disynthon_data", False):
-#             disynthon_data, disynth_exp_dict = cube.disynthonize()
-#             cube.data = disynthon_data
-#         if flags.get("polyO", False):
-#             cube.PolyO()
-#         if flags.get("top_disynthons", False):
-#             comparison_type = flags["top_disynthons"].get("comparison", "control")
-#             exp_name = flags["top_disynthons"].get("exp_name", "None")
-#             exp2_name = flags["top_disynthons"].get("exp2_name", "None")
-#             control_name = flags["top_disynthons"].get("control_name", "None")
-#             top_count = int(flags["top_disynthons"].get("top_count", 10))
-#             comparison_metric = flags["top_disynthons"].get("comparison_metric", "avg")
-#             top_disynthons_dir = create_output_dir(os.path.join(output_dir, "top_disynthons"))
-#             cube.get_top_disynthons(
-#                 disynthon_data=disynthon_data,
-#                 exp_name1=exp_name,
-#                 comparison_type=comparison_type,
-#                 exp_name2=exp2_name,
-#                 control_name=control_name,
-#                 comparison_metric=comparison_metric,
-#                 top_count=top_count,
-#                 output_dir=top_disynthons_dir,
-#             )
-#         if flags.get("trisynthon_overlap", False):
-#             trisynthon_dir = create_output_dir(os.path.join(output_dir, "trisynthon"))
-#             cube.trisynthon_overlap(output_dir=trisynthon_dir)
-#         if flags.get("disynthon_overlap", False):
-#             disynthon_dir = create_output_dir(os.path.join(output_dir, "disynthon"))
-#             cube.disynthon_overlap(
-#                 output_dir=disynthon_dir,
-#                 disynthon_data=disynthon_data,
-#                 disynth_exp_dict=disynth_exp_dict,
-#                 threshold=int(flags.get("disynthon_threshold", 20)),
-#             )
-#         if flags.get("normalized_data", False):
-#             cube.normalize()
-#         if flags.get("simple_spotfire_version", False):
-#             spotfire = cube.simple_spotfire_version()
-#             today_date = datetime.datetime.now().strftime("%Y%m%d")
-#             spotfire.to_csv(os.path.join(output_dir, f"spotfire_{today_date}.csv"), index=False)
-#         if flags.get("ml_fingerprints_to_RF_reg", False):
-#             ml_fingerprints_to_RF_dir = create_output_dir(
-#                 os.path.join(output_dir, "ml_fingerprints_to_RF")
-#             )
-#             cube.ml_fingerprints_to_RF(output_dir=ml_fingerprints_to_RF_dir)
-#         if flags.get("ml_fingerprints_to_RF_clf", False):
-#             ml_fingerprints_to_RF_clf_dir = create_output_dir(
-#                 os.path.join(output_dir, "ml_fingerprints_to_clf")
-#             )
-#             cube.ml_fingerprints_to_classifier(
-#                 output_dir=ml_fingerprints_to_RF_clf_dir,
-#                 threshold=int(flags.get("clf_thresh", 10)),
-#             )
-#         if flags.get("gnn_classifier", False):
-#             gnn_dir = create_output_dir(os.path.join(output_dir, "gnn"))
-#             cube.gnn_classifier(
-#                 output_dir=gnn_dir,
-#                 threshold=int(flags.get("gnn_threshold", 10)),
-#                 arch=flags.get("gnn_arch", "GAT"),
-#             )
-#         if "top_hits" in flags:
-#             top_hits_dir = create_output_dir(os.path.join(output_dir, "top_hits"))
-#             cube.top_n_compounds(
-#                 int(flags["top_hits"]),
-#                 flags.get("top_hits_metric", "sum"),
-#                 output_dir=top_hits_dir,
-#             )
-#         if flags.get("report", False):
-#             nsc_max_dict = nsc_max_dict if flags.get("SD_min", False) else None
-#             sd_min_dict = sd_min_dict if flags.get("SD_min", False) else None
-#             sampling_depth_dict = sampling_depth_dict if flags.get("SD_min", False) else None
-#             generate_report(
-#                 output_dir_base,
-#                 indexes,
-#                 control_cols,
-#                 nsc_max_dict,
-#                 sd_min_dict,
-#                 sampling_depth_dict,
-#             )
-#             today_date = datetime.datetime.now().strftime("%Y%m%d")
-#             cube.data.to_csv(
-#                 os.path.join(output_dir_base, f"cube_data_{today_date}.csv"),
-#                 index=False,
-#             )
+
+@cli.command(name="analyze")
+@click.option(
+    "--config",
+    type=click.Path(exists=True, dir_okay=False),
+    required=True,
+    help="Path to the YAML config file",
+)
+def analyze(config):
+    """
+    Perform DEL analysis based on the provided YAML configuration file.
+
+    CONFIG is the path to the YAML configuration file.
+    """
+    print("Analysis started with config:", config)
+
+    def create_output_dir(output_dir):
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+        return output_dir
+
+    def create_dated_output_dir(base_output_dir, name_suffix=""):
+        output_dir = create_output_dir(base_output_dir)
+        date_str = datetime.datetime.now().strftime("%Y%m%d")
+        dated_output_dir = os.path.join(output_dir, f"{date_str}_{name_suffix}")
+        if not os.path.exists(dated_output_dir):
+            os.makedirs(dated_output_dir)
+        return dated_output_dir
+
+    def load_config(config_path):
+        with open(config_path, "r") as file:
+            return yaml.safe_load(file)
+
+    def read_dict_from_file(file_path):
+        with open(file_path, "r") as f:
+            content = f.read()
+            return ast.literal_eval(content)
+
+    config = load_config(config)
+    output_dir_base = config["general"].get("output_dir", "output")
+    output_dir = create_dated_output_dir(output_dir_base, "analysis")
+
+    data_file = config["general"].get("data", "")
+    if not data_file:
+        raise ValueError("Data file must be specified in the YAML config.")
+    df = pd.read_csv(data_file)
+
+    id_col = config["general"].get("ID_col", df.columns[0])
+    indexes = ast.literal_eval(str(config.get("indexes", {})))
+    control_cols = ast.literal_eval(str(config.get("control_cols", {})))
+    raw_indexes = ast.literal_eval(str(config.get("raw_indexes", {})))
+
+    indexes_path = os.path.join(output_dir, "indexes.txt")
+    control_cols_path = os.path.join(output_dir, "control_cols.txt")
+    raw_indexes_path = os.path.join(output_dir, "raw_indexes.txt")
+
+    with open(indexes_path, "w") as file:
+        file.write(str(indexes))
+    with open(control_cols_path, "w") as file:
+        file.write(str(control_cols))
+    with open(raw_indexes_path, "w") as file:
+        file.write(str(raw_indexes))
+
+    indexes = read_dict_from_file(indexes_path)
+    control_cols = read_dict_from_file(control_cols_path)
+    raw_indexes = read_dict_from_file(raw_indexes_path)
+    if not indexes:
+        indexes = raw_indexes
+
+    cube = DELi_Cube(
+        df,
+        id_col,
+        indexes,
+        control_cols,
+        int(config["general"].get("lib_size", 0)),
+        raw_indexes,
+    )
+
+    if "flags" in config:
+        flags = config["flags"]
+        if flags.get("SD_min", False):
+            nsc_max_dict, sd_min_dict, sampling_depth_dict = cube.SD_min()
+        if flags.get("NSC_values", False):
+            cube.NSC_values()
+        if flags.get("MLE", False):
+            cube.maximum_likelihood_enrichment_ratio()
+        if flags.get("Z_score", False):
+            cube.z_score()
+        if flags.get("z_score_log_data", False):
+            cube.z_score_log_data()
+        if flags.get("disynthon_data", False):
+            disynthon_data, disynth_exp_dict = cube.disynthonize()
+            cube.data = disynthon_data
+        if flags.get("polyO", False):
+            cube.PolyO()
+        if flags.get("top_disynthons", False):
+            comparison_type = flags["top_disynthons"].get("comparison", "control")
+            exp_name = flags["top_disynthons"].get("exp_name", "None")
+            exp2_name = flags["top_disynthons"].get("exp2_name", "None")
+            control_name = flags["top_disynthons"].get("control_name", "None")
+            top_count = int(flags["top_disynthons"].get("top_count", 10))
+            comparison_metric = flags["top_disynthons"].get("comparison_metric", "avg")
+            top_disynthons_dir = create_output_dir(os.path.join(output_dir, "top_disynthons"))
+            cube.get_top_disynthons(
+                disynthon_data=disynthon_data,
+                exp_name1=exp_name,
+                comparison_type=comparison_type,
+                exp_name2=exp2_name,
+                control_name=control_name,
+                comparison_metric=comparison_metric,
+                top_count=top_count,
+                output_dir=top_disynthons_dir,
+            )
+        if flags.get("trisynthon_overlap", False):
+            trisynthon_dir = create_output_dir(os.path.join(output_dir, "trisynthon"))
+            cube.trisynthon_overlap(output_dir=trisynthon_dir)
+        if flags.get("disynthon_overlap", False):
+            disynthon_dir = create_output_dir(os.path.join(output_dir, "disynthon"))
+            cube.disynthon_overlap(
+                output_dir=disynthon_dir,
+                disynthon_data=disynthon_data,
+                disynth_exp_dict=disynth_exp_dict,
+                threshold=int(flags.get("disynthon_threshold", 20)),
+            )
+        if flags.get("normalized_data", False):
+            cube.normalize()
+        if flags.get("simple_spotfire_version", False):
+            spotfire = cube.simple_spotfire_version()
+            today_date = datetime.datetime.now().strftime("%Y%m%d")
+            spotfire.to_csv(os.path.join(output_dir, f"spotfire_{today_date}.csv"), index=False)
+        if flags.get("ml_fingerprints_to_RF_reg", False):
+            ml_fingerprints_to_RF_dir = create_output_dir(
+                os.path.join(output_dir, "ml_fingerprints_to_RF")
+            )
+            cube.ml_fingerprints_to_RF(output_dir=ml_fingerprints_to_RF_dir)
+        if flags.get("ml_fingerprints_to_RF_clf", False):
+            ml_fingerprints_to_RF_clf_dir = create_output_dir(
+                os.path.join(output_dir, "ml_fingerprints_to_clf")
+            )
+            cube.ml_fingerprints_to_classifier(
+                output_dir=ml_fingerprints_to_RF_clf_dir,
+                threshold=int(flags.get("clf_thresh", 10)),
+            )
+        if flags.get("gnn_classifier", False):
+            gnn_dir = create_output_dir(os.path.join(output_dir, "gnn"))
+            cube.gnn_classifier(
+                output_dir=gnn_dir,
+                threshold=int(flags.get("gnn_threshold", 10)),
+                arch=flags.get("gnn_arch", "GAT"),
+            )
+        if "top_hits" in flags:
+            top_hits_dir = create_output_dir(os.path.join(output_dir, "top_hits"))
+            cube.top_n_compounds(
+                int(flags["top_hits"]),
+                flags.get("top_hits_metric", "sum"),
+                output_dir=top_hits_dir,
+            )
+        if flags.get("report", False):
+            nsc_max_dict = nsc_max_dict if flags.get("SD_min", False) else None
+            sd_min_dict = sd_min_dict if flags.get("SD_min", False) else None
+            sampling_depth_dict = sampling_depth_dict if flags.get("SD_min", False) else None
+            generate_report(
+                output_dir_base,
+                indexes,
+                control_cols,
+                nsc_max_dict,
+                sd_min_dict,
+                sampling_depth_dict,
+            )
+            today_date = datetime.datetime.now().strftime("%Y%m%d")
+            cube.data.to_csv(
+                os.path.join(output_dir_base, f"cube_data_{today_date}.csv"),
+                index=False,
+            )
