b7370f89887e06e3ef31b9050ea107727c0bf998
James Wellnitz <wellnitz.james@gmail.com>
2025-10-23 19:36:43 -0400

    bump from pre-patch to patch (0.1.1a to 0.1.1)

--- docs/conf.py
+++ docs/conf.py
@@ -6,8 +6,8 @@ copyright = "2025, James Wellnitz"
 author = "James Wellnitz"
 
 # The full version, including alpha/beta/rc tags
-release = "0.1.0"
-version = "0.1.0"
+release = "0.1.1"
+version = "0.1.1"
 
 # Add any Sphinx extension module names here, as strings
 extensions = [
--- src/deli/__init__.py
+++ src/deli/__init__.py
@@ -1,3 +1,3 @@
 """deli init file"""
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
